31e3be0f2cef5dde376bbc21f92ef5904c66fd4d
zackramjan <zack.hr@gmail.com>
2019-09-18 18:01:15 +0000

    tested complete

---
+++ Ldap.py
@@ -0,0 +1,69 @@
+'''
+Created on Sep 17, 2019
+
+@author: zack.ramjan
+'''
+from ldap3 import Server, Connection, ALL
+from ldap3.extend.microsoft.addMembersToGroups import ad_add_members_to_groups as addUsersInGroups
+from ldap3.extend.microsoft.removeMembersFromGroups import ad_remove_members_from_groups as removeUsersInGroups
+import sys
+
+class Ldap(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, binddnIn,bindpwIn,serverIn,baseIn,debug=False):
+        self.binddn = binddnIn
+        self.bindpw = bindpwIn
+        self.server = serverIn
+        self.base = baseIn
+        self.debug=debug
+        self.conn = Connection(Server(self.server, get_info=ALL), self.binddn, self.bindpw, auto_bind=True)
+        self.printDebug("connected to " + self.server + " as " + self.conn.extend.standard.who_am_i())
+    
+    
+    def searchGroup(self,group):
+        self.conn.search(self.base, '(&(objectclass=group)(cn=' + group + '))')
+        if (len(self.conn.response) > 4):
+            self.printDebug("multple groups match, exiting " + str(len(self.conn.response)))
+            sys.exit()
+        if "dn" in  self.conn.response[0]:    
+            self.printDebug("found group " + self.conn.response[0]["dn"])
+            return (self.conn.response[0]["dn"])
+        else:
+            self.printDebug("could not locate group")
+            sys.exit()    
+
+    def searchUsername(self,username):
+        self.conn.search(self.base, '(&(objectclass=person)(sAMAccountName=' + username + '))')
+        if (len(self.conn.response) > 4):
+            self.printDebug("multiple username match, exiting " + str(len(self.conn.response)))
+            sys.exit()
+        if "dn" in  self.conn.response[0]:    
+            self.printDebug("found username " + self.conn.response[0]["dn"])
+            return (self.conn.response[0]["dn"])
+        else:
+            self.printDebug("could not locate username")
+            sys.exit()
+            
+    def getMembers(self,groupdn):
+        self.printDebug(groupdn)
+        self.conn.search(groupdn,'(objectclass=group)',attributes=['member'] )
+        #for m in  self.conn.response[0]['attributes']['member']:
+            #self.printDebug("\t" + m)
+        return self.conn.response[0]['attributes']['member']
+
+    def addUserToGroup(self,userdn,groupdn):
+        self.printDebug("adding " + userdn + " to " + groupdn)
+        addUsersInGroups(self.conn,[userdn,],[groupdn,], raise_error=True)
+        
+    def removeUsersInGroups(self,userdn,groupdn):
+        self.printDebug("removing " + userdn + " from " + groupdn)
+        removeUsersInGroups(self.conn,[userdn,],[groupdn,], fix=True, raise_error=True)    
+    
+    
+    def printDebug(self,string):
+        if(self.debug):
+            sys.stderr.write(string + "\n")
---
+++ ldapmgr.py
@@ -0,0 +1,60 @@
+'''
+Created on Sep 16, 2019
+
+@author: zack.ramjan
+'''
+
+import sys
+import Ldap
+import getpass 
+import argparse
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    subparsers = parser.add_subparsers(help='commands', dest="command")
+    # A list command
+    list_parser = subparsers.add_parser('list', help='List members of group')
+    list_parser.add_argument('group', action='store', help='group to list members of')
+    
+    # the add command
+    create_parser = subparsers.add_parser('add', help='add user to group')
+    create_parser.add_argument('user', action='store', help='user to add, ex: john.smith')
+    create_parser.add_argument('group', action='store', help='group to add user to, ex: staffmembers',)
+    
+    # the delete command
+    delete_parser = subparsers.add_parser('remove', help='Remove user from group')
+    delete_parser.add_argument('user', action='store', help='user to remove, ex: john.smith')
+    delete_parser.add_argument('group', action='store', help='group to remove user from, ex: staffmembers',)
+    args = parser.parse_args()
+    
+    if not args.command: 
+        parser.print_help()
+        sys.exit() 
+    
+    binddn = input("username: ")
+    if "@vai.org" not in binddn.lower():
+        binddn += "@vai.org" 
+    bindpw=getpass.getpass(prompt='Password: ', stream=None) 
+    server='vai.org'
+    base='dc=vai,dc=org'
+    myLdap = Ldap.Ldap( binddn,bindpw,server,base,debug=True)
+    
+    
+    if args.command == 'list':
+        print ("listing " + args.group)
+        groupdn = myLdap.searchGroup(args.group)
+        for m in myLdap.getMembers(groupdn):
+            print("\t" + m) 
+    
+    if args.command == 'add':
+        print ("adding " + args.user + " to " + args.group)
+        groupdn = myLdap.searchGroup(args.group)
+        userdn = myLdap.searchUsername(args.user)
+        myLdap.addUserToGroup(userdn,groupdn)
+   
+    if args.command == 'remove':
+        print ("removing " + args.user + " from " + args.group)
+        groupdn = myLdap.searchGroup(args.group)
+        userdn = myLdap.searchUsername(args.user)
+        myLdap.removeUsersInGroups(userdn,groupdn)
+   
